762c95a4cafdc0096498b8f25addc1817f45d48b
Daniele Sluijters <github@daenney.net>
2014-03-12 23:49:34 +0100

    Fetch loglevel from settings and configure logger.

--- glanerbeard/web.py
+++ glanerbeard/web.py
@@ -12,6 +12,11 @@ app = Flask(__name__)
 app.config.from_object('glanerbeard.default_settings')
 app.config.from_envvar('GLANERBEARD_SETTINGS')
 
+numeric_level = getattr(logging, app.config['LOGLEVEL'].upper(), None)
+if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % loglevel)
+logging.basicConfig(level=numeric_level)
+log = logging.getLogger(__name__)
 
 servers = Server.createFromConfig(app.config['SERVERS'], app.config['API_KEYS'])
 
